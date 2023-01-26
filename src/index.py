a8be34db8b6edb4c447c71d5c1ada0193996b3c4
daveth <davethsite@gmail.com>
2023-01-26 21:32:51 +0100

    First version of the complete work

---
+++ src/index.py
@@ -0,0 +1,31 @@
+# a = 5
+# b = 6
+# c = a + b
+
+# print(c)
+
+# if(a==b):
+#     print('a is equal to b')
+# else:
+#     print('a is not equal to b')
+
+# names = ['Ovie', 'Unyime', 'Ididie', 'Ubong', 'Abadie']
+
+# i = 0
+# while i < 3:
+#     print(names[i])
+#     i = i + 1
+
+# for name in names:
+#     print(names)
+
+a = 1 #integer
+name = 'Ovie' #string
+num = 3.5 #float
+num = 6
+ismyName = True #boolean
+
+if ismyName == True:
+    print(num)
+else:
+    print(name)
